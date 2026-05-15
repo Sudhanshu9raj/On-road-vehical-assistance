0d45db031044792f80ba406d2de0f20e9b4d4541
Sudhanshu9raj <175186540+Sudhanshu9raj@users.noreply.github.com>
2026-05-16 01:30:19 +0530

    fixed render deployment

--- backend/app.py
+++ backend/app.py
@@ -294,4 +294,11 @@ def handle_message(data):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True, port=5000, allow_unsafe_werkzeug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port
+    )
